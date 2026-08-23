d1144f34497989da2a39e71fd429450f75227b7f
Marco Schmidt <48087020+SchmidtWebmedia@users.noreply.github.com>
2026-08-23 09:29:16 +0200

    Make donation endpoint path site-aware

--- Classes/Controller/DonationController.php
+++ Classes/Controller/DonationController.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace Schmidtwebmedia\SepaDonate\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class DonationController extends ActionController
 {
+    private const ENDPOINT_PATH = '/api/sepa-donate/qr-code';
+
     public function formAction(): ResponseInterface
     {
-        $this->view->assign('amounts', $this->getAmounts());
+        $this->view->assignMultiple([
+            'amounts' => $this->getAmounts(),
+            'endpointPath' => $this->getEndpointPath(),
+        ]);
 
         return $this->htmlResponse();
     }
@@ -22,5 +28,15 @@ class DonationController extends ActionController
             'intval',
             explode(',', $this->settings['button_bar_amounts'] ?? '10,25,50,100')
         );
+    }
+
+    private function getEndpointPath(): string
+    {
+        $site = $this->request->getAttribute('site');
+        if (!$site instanceof Site) {
+            return self::ENDPOINT_PATH;
+        }
+
+        return rtrim($site->getBase()->getPath(), '/') . self::ENDPOINT_PATH;
     }
 }
